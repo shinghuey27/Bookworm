b68e48c957f50f256900e58fa9b5b3e3e680dadd
shinghuey27 <shinghuey27@hotmail.com>
2024-06-06 22:21:22 +0800

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Shing Huey on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
